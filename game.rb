6c943e88837bdcbb570f614fb057f6d7bd8b2bab
Kian <Ksadeghi@sas.upenn.edu>
2020-08-26 16:31:49 -0400

    compartmenalized different functions

--- game.rb
+++ game.rb
@@ -1,20 +1,38 @@
 require_relative 'board'
+require_relative 'player'
 
 class Game
 
-  attr_accessor :rand_selected_word, :board
+  attr_reader :rand_selected_word, :board, :player
 
   def initialize
     @rand_selected_word = File.open('5desk.txt', 'r').readlines.sample.chomp
-    @board = Board.new(self.rand_selected_word)
+    @board = Board.new(rand_selected_word)
+    @player = Player.new
   end
 
-  def main_game_loop
-    puts "Hi! Welocme to hangman."
-    self.board.print_board
+  def start_game
+    puts "\nHi! Welcome to hangman. The computer just thought of a word that you will
+have to guess. Here is the board: "
+    board.print_board
+    puts "Please insert your guess as a single character like \"a\". Enjoy!\n "
+    main_game_loop
   end
+
+
+  def main_game_loop
+    if player.make_guess
+      puts "Great! Beep Boop. Here is the new board:"
+    else
+      puts "Hmm. That's not a character! Try again."
+    end
+    
+    #board.contains_character?(guess)
+
+  end
+
 end
 
 
 game = Game.new
-game.main_game_loop
+game.start_game
