f161f1fb47a0f88dbe1303b0391046776dba6f54
Kian <Ksadeghi@sas.upenn.edu>
2020-08-26 18:14:33 -0400

    Informs user of repeated inputs

--- player.rb
+++ player.rb
@@ -1,21 +1,18 @@
 class Player
 
-  attr_accessor :guesses_left
+  attr_accessor :guesses_left, :already_guessed
 
   def initialize
     @guesses_left = 10
+    @already_guessed = []
   end
 
   def make_guess
     guess = gets.chomp
+  end
+  def valid_guess(guess)
     guess.ord >= 65 && guess.ord <= 90 && guess.length == 1 ||
       guess.ord >= 97 && guess.ord <= 122 && guess.length == 1
   end
-
-
-
-
-
-
 end
 
