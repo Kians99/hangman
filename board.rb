4c20974733f8eb7fc50d86b2b325eb48f267c70e
Kian <Ksadeghi@sas.upenn.edu>
2020-08-26 18:12:51 -0400

    Adds correct matches to the screen

--- board.rb
+++ board.rb
@@ -3,23 +3,28 @@ require_relative 'letter'
 
 class Board
 
-  attr_accessor :letters, :board_print_out
+  attr_accessor :letters, :board_print_out, :hidden_word
 
   def initialize(hidden_word)
     @hidden_word = hidden_word
-    @letters = hidden_word.chars
     @board_print_out = Array.new(hidden_word.length, '_ ')
   end
 
   def print_board
     puts "\n"
-    print board_print_out
+    board_print_out.each { |char| print char }
     puts "\n"
   end
 
+  def change_board(array_of_indices, char_inp) 
+    array_of_indices.each { |index| board_print_out[index] = char_inp + " " }
+    print_board
+  end
+  
+  def contains_character?(char_inp)
+    hidden_word.chars.each_index.select{ |indx| hidden_word.chars[indx] == char_inp }
+  end
+    
+
 end
 
-def contains_character?(char_inp)
-  index = hidden_word.indexOf(char_inp)
-  board_print_out[index] = char_inp if index != -1
-end
