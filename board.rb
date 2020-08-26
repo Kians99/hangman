0b7c5f9dde97adf1fec9e18c47abcafccaa36715
Kian <Ksadeghi@sas.upenn.edu>
2020-08-26 16:31:27 -0400

    Checks to see if user guessed a correct letter

--- board.rb
+++ board.rb
@@ -3,25 +3,23 @@ require_relative 'letter'
 
 class Board
 
-  attr_accessor :letters
+  attr_accessor :letters, :board_print_out
 
   def initialize(hidden_word)
-    @letters = letter_array(hidden_word) 
+    @hidden_word = hidden_word
+    @letters = hidden_word.chars
+    @board_print_out = Array.new(hidden_word.length, '_ ')
   end
 
   def print_board
     puts "\n"
-    puts '_ ' * letters.length
+    print board_print_out
     puts "\n"
   end
 
-  private
-
-  def letter_array(hidden_word)
-    hidden_word.chars.map do |letter|
-      Letter.new(letter)
-    end
-  end
 end
 
-board = Board.new("hello")
+def contains_character?(char_inp)
+  index = hidden_word.indexOf(char_inp)
+  board_print_out[index] = char_inp if index != -1
+end
