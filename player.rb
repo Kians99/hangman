d22683169680780b440cc00631b269fc9fa08612
Kian <Ksadeghi@sas.upenn.edu>
2020-08-28 15:07:08 -0400

    Gives player two times the length of the hidden word guesses

--- player.rb
+++ player.rb
@@ -2,15 +2,21 @@ class Player
 
   attr_accessor :guesses_left, :already_guessed
 
-  def initialize
-    @guesses_left = 10
+  def initialize(length_of_hidden_word)
+    @guesses_left = length_of_hidden_word * 2
     @already_guessed = []
   end
 
   def make_guess
-    guess = gets.chomp
+    gets.chomp
   end
+
+  def add_guess(guess)
+    already_guessed.push(guess)
+  end
+
   def valid_guess(guess)
+    return false if guess == ""
     guess.ord >= 65 && guess.ord <= 90 && guess.length == 1 ||
       guess.ord >= 97 && guess.ord <= 122 && guess.length == 1
   end
